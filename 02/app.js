5584114f56e18c2872819df3b27b8eb5e2aabaf6
Artur Matysiak <inz.matys@gmail.com>
2023-12-06 16:27:59 +0100

    task done

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,28 @@
-
 /* rozwiązanie z pętlą for */
-const x = 4;
+// const x = 4;
 
+const multiply = () => {
+  const x = prompt("Podaj liczę: ");
+  if (x >= 0 && x <= 9) {
+    for (i = 0; i <= 9; i++) {
+      console.log(x * i);
+    }
+  } else console.log("za duża liczba", multiply());
+};
 
+// multiply();
 
 /* rozwiązanie z pętlą while  */
+
+const a = 5;
+const n = 3;
+
+result = () => {
+  let sum = 1;
+  for (i = 1; i <= n; i++) {
+    sum = sum * a;
+  }
+  console.log(sum);
+};
+
+result();
